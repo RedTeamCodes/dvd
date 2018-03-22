b26d4a3d6e2e41da75d5df467612d864ac88635f
Alexander A Pogrebinsky <alex@pogrebinskygroup.com>
2018-03-22 11:47:21 -0400

    Working on the FINDALLDVD.SC

--- DVDStore.WEB/DVDStore.Access/Methods/FindAllDVDs.cs
+++ DVDStore.WEB/DVDStore.Access/Methods/FindAllDVDs.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DVDStore.Data.Models;
+
 
 namespace DVDStore.Access.Methods
 {
     public class FindAllDVDs
     {
+
+        private DVDStoreContext db = new DVDStoreContext();
+
+
         public void FindAllDVD()
         {
+            var FindDVDS = new List<string>();
 
+            var DVDQuery = from d in db.DVD
+                           orderby d.Title
+                           select d.Title;
+
+            FindDVDS.AddRange(DVDQuery.Distinct());
+            ViewBag.
         }
     }
 }
